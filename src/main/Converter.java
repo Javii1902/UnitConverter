4537c5f50c38492eec850544d14e9218ed26202c
Javii1902 <57228962+Javii1902@users.noreply.github.com>
2021-08-14 00:57:51 -0500

    Created new methods for different conversions. Completed all methods for conversion. Also completed the main function. All left to do now is clean up.

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -16,44 +16,52 @@ public class Converter {
 		menuSelection = getUserChoice(uChoice);
 		
 		//first while loop. keeps going until choice = 5
-		while(menuSelection !=5) {
+		while(menuSelection != 6) {
 			
-			//while loop to make sure choice is in right range 
-			/*
-			 * while(menuSelection < 1 || menuSelection > 5) {
-			 * //System.out.println("inside nested loop6"); if (menuSelection < 1 ||
-			 * menuSelection > 5) { System.out.println("number is not in range");
-			 * menuSelection = getUserChoice(uChoice); } else break; }
-			 */
 			
 			switch (menuSelection) {
+			//case 1 temperature
 			case 1:
 				System.out.println("You chose temperature conversion");
+				System.out.println("Please enter temperature in Farenheit");
+				System.out.println(farenheitToCelcius(uChoice.nextDouble()) + " degrees celcius");
 				break;
+				
+			//case 2 distance
 			case 2:
 				System.out.println("You chose distance conversion");
+				System.out.println("Please enter distance in miles");
+				System.out.println(milesToKilometers(uChoice.nextDouble()) + " kilometers");
 				break;
+			//case 3 money
 			case 3:
 				System.out.println("You chose money conversion");
+				System.out.println("Please enter dollar amount");
+				System.out.println(dollarsToPounds(uChoice.nextDouble()) + " pounds");
 				break;
+			//case 4 volume
 			case 4:
 				System.out.println("You chose Volume conversion");
+				System.out.println("Please enter milliliter amount");
+				System.out.println(mlToOz(uChoice.nextDouble()) + " ounces");
 				break;
+			//case 5 exit loop
 			case 5:
+				System.out.println("You chose weight conversion");
+				System.out.println("Please enter mass");
+				System.out.println(kilogramsToPounds(uChoice.nextDouble()) + " pounds");
 				break;
+			case 6:
+				break;
+			//defaults if number is out of range
 			default:
 				System.out.println("number is not in range");
 			}
-			
-			System.out.println("Outside of nested loop");
+			//System.out.println("Outside of nested loop");
 			showMenu();	
 			menuSelection = getUserChoice(uChoice);
-//			if (menuSelection == 5)
-//				break;
 		}
-		
-		
-		System.out.println("End of program");
+		System.out.println("Thank you for using this unit converter!");
 		uChoice.close();
 	}
 	
@@ -66,7 +74,8 @@ public class Converter {
 		System.out.println("2. Distance");
 		System.out.println("3. Money");
 		System.out.println("4. Volume");
-		System.out.println("5. quit");
+		System.out.println("5. Weight");
+		System.out.println("6. quit");
 	}
 
 	
@@ -78,22 +87,29 @@ public class Converter {
 		return menuSelection;
 	}
 	//Convert temperature
-	public static double farenheitToCelcius(double temp){
-		temp = ((temp - 32)*(5/9));
-		return temp;
+	public static double farenheitToCelcius(double farenheit){
+		farenheit = (((farenheit - 32)*5)/9);
+		return farenheit;
 	}
 	//Convert Distance
 	public static double milesToKilometers(double distance) {
+		distance = distance * 1.609344;
 		return distance;
 	}
 	//Convert Money
-	public static double dollersToPounds(double dollers) {
-		return dollers;
+	public static double dollarsToPounds(double dollars) {
+		dollars = dollars * .72;
+		return dollars;
 	}
 	//Convert Weight
-	
-	//Convert Speed
-	
-	
+	public static double kilogramsToPounds(double kilograms) {
+		kilograms = kilograms * 2.205;
+		return kilograms;
+	}
+	//Convert volume
+	public static double mlToOz(double ml) {
+		ml = ml / 29.574;
+		return ml;
+	}
 	
 }
